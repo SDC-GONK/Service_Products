b893e33a6cf4beb2956ac0f7e60c6c38cf53e70e
Jerry Tapia <jerryxtapia@gmail.com>
2022-06-08 15:54:33 -0700

    fixed postgres schema so that it coincides with incoming data

--- schemas/postgres_schema.sql
+++ schemas/postgres_schema.sql
@@ -16,9 +16,9 @@ create table if not exists products(
 drop table if exists product_information CASCADE;
 create table if not exists product_information(
     id serial primary key,
-    feature varchar(255) not null not null,
-    value varchar(255) not null not null,
-    product_id int not null not null,
+    product_id int not null,
+    feature varchar(255) not null,
+    value varchar(255) not null,
     foreign key(product_id) references products(id)
         on delete cascade on update cascade
 );
@@ -26,44 +26,43 @@ create table if not exists product_information(
 drop table if exists product_styles CASCADE;
 create table if not exists product_styles(
     id serial primary key,
-    name varchar(255) not null,
-    original_price int not null,
-    sale_price int,
-    default_style bool,
     product_id int,
+    name varchar(255) not null,
+    original_price varchar(100),
+    sale_price varchar(100),
+    default_style bool,
     foreign key(product_id) references products(id)
         on delete cascade on update cascade
 );
 
 drop table if exists related_products CASCADE;
 create table if not exists related_products(
+    id serial primary key,
     parent_product int,
     related_product int,
     foreign key(parent_product) references products(id)
-        on delete cascade on update cascade,
-    foreign key(related_product) references products(id)
         on delete cascade on update cascade
 );
 
 drop table if exists photos CASCADE;
 create table if not exists photos(
-    photo_id int,
-    thumbnail_url varchar(255),
-    url varchar(255),
+    id serial primary key,
     product_styles_id int,
+    url text,
+    thumbnail_url text,
     foreign key(product_styles_id) references product_styles(id)
         on delete cascade on update cascade
 );
 
-DROP TYPE IF EXISTS sizes CASCADE;
-create TYPE sizes AS ENUM('XS', 'S', 'M', 'L', 'XL', 'XXL');
+-- DROP TYPE IF EXISTS sizes CASCADE;
+-- create TYPE sizes AS ENUM('XS', 'S', 'M', 'L', 'XL', 'XXL');
 
 drop table if exists skus CASCADE;
 create table if not exists skus(
-    sku_id varchar(100) not null,
-    quantity int,
-    current_size sizes not null,
+    id serial primary key,
     product_styles_id int,
+    size varchar(25) not null,
+    quantity int,
     foreign key(product_styles_id) references product_styles(id)
         on delete cascade on update cascade
 );
